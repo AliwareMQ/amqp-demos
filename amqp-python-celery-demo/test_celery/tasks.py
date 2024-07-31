61b82e0908b28ad0d55e1775e37f0cbc67ccd522
shannon.dl <shannon.dl@alibaba-inc.com>
2024-07-31 16:50:15 +0800

    Use static password in python celery demo instead of ak/sk

--- amqp-python-celery-demo/test_celery/tasks.py
+++ amqp-python-celery-demo/test_celery/tasks.py
@@ -5,8 +5,8 @@ import time
 
 @app.task
 def longtime_add(x, y):
-    print 'long time task begins'
-    # sleep 5 seconds
-    time.sleep(1)
-    print 'long time task finished'
+    print('long time task begins')
+    # sleep 2 seconds
+    time.sleep(2)
+    print('long time task finished')
     return x + y

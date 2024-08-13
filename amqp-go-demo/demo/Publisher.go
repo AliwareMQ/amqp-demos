e6fd27cea9bc5646226633c54146de3d13797a29
ShannonDing <libya_003@163.com>
2024-08-13 23:10:53 +0800

    Use username/pwd and go mod for new rabbit go demo

--- amqp-go-demo/demo/Publisher.go
+++ amqp-go-demo/demo/Publisher.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/streadway/amqp"
 	"log"
+	"time"
 )
 
 func main() {
 
 	var buf bytes.Buffer
 
-	userName := "username"                           // 请替换成您阿里云AMQP控制台Username
-	password := "password"                           // 请替换成您阿里云AMQP控制台Username
-	endpoint := "rabbitmq-xxxx.mq.amqp.aliyuncs.com" // 请从控制台获取。
-	vhost := "your-vhost"                            // 请从控制台获取。
-	exchangeName := "your-exchangeName"              // 请从控制台获取。
+	userName := "username"                          // 请替换成您阿里云AMQP控制台Username
+	password := "password"                          // 请替换成您阿里云AMQP控制台Username
+	endpoint := "rabbitmq-xxx.mq.amqp.aliyuncs.com" // 请从控制台获取。
+	vhost := "your-vhost"                           // 请从控制台获取。
+	exchangeName := "your-exchangeName"             // 请从控制台获取。
 	exchangeType := "direct"
 	queueName := "your-queueName"  // 请从控制台获取。
 	routeKey := "your-routing-key" // 请从控制台获取。
@@ -58,18 +60,24 @@ func main() {
 	 * rabbitmq client 向Server发起connection,新建channel大约需要进行15+个TCP报文的传输，会消耗大量网络资源和Server端的资源，甚至引起Server端SYN flooding 攻击保护。
 	 * 因此我们建议消息的发送和消费尽量采用长链接的模式。
 	 */
-	body := "Hello World!"
-	err = ch.Publish(
-		exchangeName, // exchangeName
-		routeKey,     // routing key
-		false,        // mandatory
-		false,        // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
+	for i := 0; i < 10; i++ {
+		body := fmt.Sprintf("Hello GO AMQP[%d]", i+1)
+		err = ch.Publish(
+			exchangeName, // exchangeName
+			routeKey,     // routing key
+			false,        // mandatory
+			false,        // immediate
+			amqp.Publishing{
+				ContentType:  "text/plain",
+				Body:         []byte(body),
+				DeliveryMode: amqp.Persistent,
+			})
 
-	failPublishOnError(err, "Failed to publish a message")
+		failPublishOnError(err, "Failed to publish a message")
+		fmt.Printf("Go Message [%d] sent and confirmed.\n", i+1)
+		time.Sleep(1 * time.Second)
+	}
+	time.Sleep(3 * time.Second)
 
 }
 

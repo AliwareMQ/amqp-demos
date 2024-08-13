3a72d26c53e9a11425c4287a1054ec84281a7a4d
ShannonDing <libya_003@163.com>
2024-08-13 16:36:36 +0800

    use user and pwd to access ampq in spring rabbit demo

--- spring-rabbit-demo/src/main/java/com/alibaba/rabbit/spring/SpringMain.java
+++ spring-rabbit-demo/src/main/java/com/alibaba/rabbit/spring/SpringMain.java
@@ -10,12 +10,14 @@ public class SpringMain {
     public static void main(final String... args) throws Exception {
 
         AbstractApplicationContext ctx =
-            new ClassPathXmlApplicationContext("spring-rabbitmq.xml");
+                new ClassPathXmlApplicationContext("spring-rabbitmq.xml");
         RabbitTemplate template = ctx.getBean(RabbitTemplate.class);
-        template.convertAndSend("Hello, world!");
-        int i=0;
-        while(i<10000){
-            Thread.sleep(1000);
+        int i = 0;
+        while (i < 10) {
+            String message = "Hello World, I am AMQP [" + i + "]!";
+            template.convertAndSend(message);
+            System.out.println(" [-] Send '" + message + "'");
+            Thread.sleep(2000);
             i++;
         }
         ctx.destroy();

6e1330437454a477fd413956edd7ce153e52d282
dinglei <libya_003@163.com>
2024-08-14 09:56:18 +0800

    use user and pwd to access ampq in spring rabbit demo (#40)

--- spring-rabbit-demo/src/main/java/com/alibaba/rabbit/spring/MessageReceiver.java
+++ spring-rabbit-demo/src/main/java/com/alibaba/rabbit/spring/MessageReceiver.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-public class Foo {
+public class MessageReceiver {
     public void listen(String message) {
         System.out.println(" [x] Received '" + message + "'");
     }

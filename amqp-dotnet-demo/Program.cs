9e301b585d8556c032f5ef31e2d28674bf968d46
Derick <charleszhn@hotmail.com>
2024-08-15 23:13:07 +0800

    add amqp for aliyun rabbitmq

---
+++ amqp-dotnet-demo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Microsoft.Extensions.Configuration;
+using RabbitMQConsoleApp;
+
+class Program
+{
+    private static IConfiguration Configuration { get; set; }
+
+    static void Main(string[] args)
+    {
+        // 加载配置
+        Configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        try
+        {
+            var rabbitMQService = new RabbitMQService(Configuration);
+            
+
+            Console.WriteLine("1. Send a message");
+            Console.WriteLine("2. Receive messages");
+            Console.Write("Choose an option: ");
+
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                rabbitMQService.SendMessage();
+            }
+            else if (option == "2")
+            {
+                rabbitMQService.ReceiveMessages();
+            }
+            else
+            {
+                Console.WriteLine("Invalid option");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        Console.WriteLine("Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
+
---
+++ amqp-dotnet-demo/readme.md
@@ -0,0 +1,5 @@
+# dotnet amqp demo
+
+1. 当前使用的dotnet版本是8.0
+2. 将MQ配置放置在专门的appsettings.json文件中
+3. 封装MQ的操作类，提供基本的发送消息，接受消息操作方法

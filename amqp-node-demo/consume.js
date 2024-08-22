d48d9328fafc0b79e31ed2a9b1083afd467b6258
Jack Tsai <tsunghanjacktsai@outlook.com>
2024-08-22 12:59:47 +0800

    Update amqp node demo (#48)
    Co-authored-by: tsaitsung-han.tht <tsaitsung-han.tht@alibaba-inc.com>

---
+++ amqp-node-demo/consume.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+const amqp = require('amqplib');
+
+const endpoint = 'endpoint';
+const username = 'username';
+const password = 'password';
+const vhost = 'vhost';
+const queue = 'queue';
+
+const amqpUrl = `amqp://${username}:${password}@${endpoint}/${vhost}`;
+
+async function consume() {
+  try {
+    // Connect to RabbitMQ Server
+    const connection = await amqp
+      .connect(amqpUrl)
+      .catch((err) => {
+        console.log(`connect error: ${err}`);
+        throw err;
+      });
+
+    // Create a channel
+    const channel = await connection.createChannel().catch((err) => {
+      console.log(`channel create error: ${err}`);
+      throw err;
+    });
+
+    // Consume message
+    channel.consume(
+      queue,
+      (msg) => {
+        if (msg !== null) {
+          console.log(" [x] Received:", msg.content.toString());
+          // Ack after the message has been processed
+          channel.ack(msg);
+        }
+      },
+      { noAck: false }
+    );
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+consume();
---
+++ amqp-node-demo/send.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const amqp = require('amqplib');
+
+const endpoint = 'endpoint';
+const username = 'username';
+const password = 'password';
+const vhost = 'vhost';
+const queue = 'queue';
+
+const amqpUrl = `amqp://${username}:${password}@${endpoint}/${vhost}`;
+
+async function connectToAmqpAndSendMsg(amqpUrl, queue, msg) {
+    try {
+        const connection = await amqp.connect(amqpUrl);
+        const channel = await connection.createChannel();
+        await channel.assertQueue(queue, { durable: true });
+        channel.publish('amq.direct','binding-test', Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+
+        setTimeout(async () => {
+            await channel.close();
+            await connection.close();
+        }, 500); // Close the connection after a delay to ensure message delivery
+    } catch (error) {
+        throw new Error(`Error connecting to AMQP or sending message: ${error.message}`);
+    }
+}
+
+async function send() {
+    try {
+        const msg = "Hello World!";
+        await connectToAmqpAndSendMsg(amqpUrl, queue, msg);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+send();

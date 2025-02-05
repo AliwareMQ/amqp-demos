9d172c29a5d101fe7d1df21bffe1735ad5351c04
rongtong <jinrongtong5@163.com>
2025-02-05 10:09:05 +0800

    Add code to enable data transmission encryption in spring cloud bus code (#63)

--- amqp-springcloudbus-demo/config-client/src/main/java/com/alibaba/rabbit/RabbitConfig.java
+++ amqp-springcloudbus-demo/config-client/src/main/java/com/alibaba/rabbit/RabbitConfig.java
@@ -33,6 +33,9 @@ public class RabbitConfig {
         rabbitConnectionFactory.setPassword(rabbitProperties.getPassword());
         rabbitConnectionFactory.setAutomaticRecoveryEnabled(true);
         rabbitConnectionFactory.setNetworkRecoveryInterval(5000);
+        // 开启数据传输加密
+//        rabbitConnectionFactory.getRabbitConnectionFactory().useSslProtocol();
+//        rabbitConnectionFactory.setPort(5671);
         ConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitConnectionFactory);
         ((CachingConnectionFactory) connectionFactory).setPublisherConfirms(rabbitProperties.isPublisherConfirms());
         ((CachingConnectionFactory) connectionFactory).setPublisherReturns(rabbitProperties.isPublisherReturns());
